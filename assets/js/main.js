e6f1053af29dfec5837600775a34ec56ab73c750
joy-dey <36854712+joy-dey@users.noreply.github.com>
2020-11-29 22:28:29 +0530

    updated category page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
-let currentLocation = window.location.pathname,
+let currentLocation = location.pathname,
+    currentPage = location.href,
     passwordInput = document.querySelector('#pwd'),
     showPass = document.querySelector('#showPass');
 setTimeout(() => {
@@ -17,6 +18,15 @@ if (showPass) {
     })
 }
 
-console.log(currentLocation);
+let productName = document.querySelectorAll('[data-productName]'),
+    shareProduct = document.querySelectorAll('[data-share]'),
+    price = document.querySelectorAll('[data-price]');
 
-// slick
+shareProduct.forEach((prd, index) => {
+    prd.addEventListener('click', () => {
+        let getName = `${productName[index].innerText}`,
+            message = `Hey I have recently found this beautiful product named ${getName} by Geeta's Collection at ${currentPage}`,
+            urlMessage = message.replace(/ /g, '%20').replace(':', '%3A').replaceAll('/', '%2F');
+        alert(`https://api.whatsapp.com/send?text=${urlMessage}`);
+    })
+});
